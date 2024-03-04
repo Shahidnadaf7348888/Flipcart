dc8730b5bfd193fe3fc437b022434a9139cec669
ShahidNadaf <105375283+Shahidnadaf7348888@users.noreply.github.com>
2024-03-04 05:26:51 +0000

    xsa

--- Flipcart01/src/Components/Mobiles.jsx
+++ Flipcart01/src/Components/Mobiles.jsx
@@ -1,4 +1,4 @@
-    import React from 'react'
+
 
 
     // Img Imports 
@@ -41,151 +41,151 @@
         <>
         <div className="container-fluide text-center p-3">
     <div className="row">
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo1} className="card-img-top pb-2 w-75 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo1} className="card-img-top pb-2 w-50 m-auto w-50 m-auto" alt=""/>
             Apple iPhone 11<h6>₹1,20,099 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,21,100</p> <b className="text-success">50% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3  align-items-center">
-            <img src={Mo2} className="card-img-top pb-2 w-75 w-75 " alt=""/>
+        <div className="col-md-3 col-6 card  mb-3  align-items-center">
+            <img src={Mo2} className="card-img-top pb-2 w-50 m-auto w-50 m-auto " alt=""/>
             Apple iPhone 11 <h6>₹1,12,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,23,300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo3} className="card-img-top  w-75 w-75 " alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo3} className="card-img-top  w-50 m-auto w-50 m-auto " alt=""/>
             Apple iPhone 12 <h6>₹2,11,799 <p style={{color: "darkgrey", fontSize: "87%"}} >₹3,11,300</p> <b className="text-success">40% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo21} className="card-img-top pb-2 w-75 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo21} className="card-img-top pb-2 w-50 m-auto w-50 m-auto" alt=""/>
                 Samsung J2<h6>₹7,399 <p style={{color: "darkgrey", fontSize: "87%"}} >₹18,000</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo14} className="card-img-top pb-2 w-75 " alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo14} className="card-img-top pb-2 w-50 m-auto " alt=""/>
             Samsung J3<h6>₹10,099 <p style={{color: "darkgrey", fontSize: "87%"}} >₹12,200</p> <b className="text-success">50% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo7} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo7} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             White & Blue iPhones<h6>₹1,12,709 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,20,900</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo8} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo8} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 <h6>₹1,23,445 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,34,800</p> <b className="text-success">70% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo9} className="card-img-top pb-4 w-50" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo9} className="card-img-top pb-4 w-25 m-auto" alt=""/>
             Samsung J5<h6>₹12,604 <p style={{color: "darkgrey", fontSize: "87%"}} >₹21,200</p> <b className="text-success">50% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 align-items-center card p-2 mb-3 ">
-            <img src={Mo10} className="card-img-top pb-2 w-50 " alt=""/>
+        <div className="col-md-3 col-6 align-items-center card  mb-3 ">
+            <img src={Mo10} className="card-img-top pb-2 w-25 " alt=""/>
             Apple iPhone 12 <h6>₹1,34,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,60,800</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 align-items-center card p-2 mb-3 ">
-            <img src={Mo11} className="card-img-top pb-2  w-50 " alt=""/>
+        <div className="col-md-3 col-6 align-items-center card  mb-3 ">
+            <img src={Mo11} className="card-img-top pb-2 pt-2 w-25 " alt=""/>
             Apple iPhone 12 <h6>₹1,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,33,999</p> <b className="text-success">50% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 align-items-center card p-2 mb-3 ">
-            <img src={Mo12} className="card-img-top pb-2 w-50 " alt=""/>
+        <div className="col-md-3 col-6 align-items-center card  mb-3   ">
+            <img src={Mo12} className="card-img-top pb-2 w-25 " alt=""/>
             Apple iPhone 12 <h6>₹1,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,33,999</p> <b className="text-success">50% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 align-items-center card  p-2 mb-3 ">
-            <img src={Mo13} className="card-img-top pb-2 w-75  " alt=""/>
+        <div className="col-md-3 col-6 align-items-center card   mb-3  ">
+            <img src={Mo13} className="card-img-top w-50   " alt=""/>
             Samsung <h6>₹12,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹13,000</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 align-items-center card p-2 mb-3 ">
-            <img src={Mo5} className="card-img-top  pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 align-items-center card mb-3   ">
+            <img src={Mo5} className="card-img-top  pb-2 w-50 m-auto" alt=""/>
             Samsung J3 <h6>₹12,399 <p style={{color: "darkgrey", fontSize: "87%"}} >₹16,000</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo15} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo15} className="card-img-top pb-2 w-75 m-auto" alt=""/>
             Apple iPhone 14<h6>₹2,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,44,1300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo16} className="card-img-top pb-2 w-50" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo16} className="card-img-top pb-2 w-50 m-auto pt-3" alt=""/>
             Apple iPhone 12 <h6>₹1,23,349 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,800</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo17} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo17} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Smart Phone <h6>₹92,349 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,23,400</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo18} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo18} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 <h6>₹1,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,200</p> <b className="text-success">30% off</b></h6> 
             
         </div>
 
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo19} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo19} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 (red) <h6>₹1,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,200</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo20} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo20} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 (Green) <h6>₹₹1,22,499  <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,200</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo4} className="card-img-top pb-3 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo4} className="card-img-top pb-3 w-50 m-auto" alt=""/>
             Apple iPhone 12 (Perpel) <h6>₹1,22,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,200</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo22} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo22} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 13 <h6>₹2499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,44,300</p> <b className="text-success">70% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo23} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo23} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 <h6>₹1,23,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,40,000</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo25} className="card-img-top pb-2 w-50 " alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo25} className="card-img-top m-auto w-25" alt=""/>
             Smart Phone <h6>₹12,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹16,000</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo26} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3 ">
+            <img src={Mo26} className="card-img-top  w-50 m-auto" alt=""/>
             Apple iPhone 7 <h6>₹12,799 <p style={{color: "darkgrey", fontSize: "87%"}} >₹22,300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card align-items-center p-2 mb-3 ">
-            <img src={Mo27} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card align-items-center  mb-3 ">
+            <img src={Mo27} className="card-img-top  w-50 m-auto" alt=""/>
             Apple iPhone 12  <h6>₹1,23,499 <p style={{color: "darkgrey", fontSize: "87%"}} >₹1,67,300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card align-items-center p-2 mb-3 ">
-            <img src={Mo28} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card align-items-center mb-3 ">
+            <img src={Mo28} className="card-img-top  w-50 m-auto" alt=""/>
             Apple iPhone 10 <h6>₹23,599 <p style={{color: "darkgrey", fontSize: "87%"}} >₹42,400</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card align-items-center p-2 mb-3 ">
-            <img src={Mo29} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card align-items-center  mb-3 ">
+            <img src={Mo29} className="card-img-top pb-2 w-50 m-auto" alt=""/>
             Apple iPhone 12 <h6>₹1,64,299 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,12,300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
-        <div className="col-md-3 col-6 card p-2 mb-3 ">
-            <img src={Mo30} className="card-img-top pb-2 w-75" alt=""/>
+        <div className="col-md-3 col-6 card  mb-3  ">
+            <img src={Mo30} className="card-img-top pb-2 w-50 m-auto " alt=""/>
             Apple iPhone 12 <h6>₹1,64,299 <p style={{color: "darkgrey", fontSize: "87%"}} >₹2,12,300</p> <b className="text-success">30% off</b></h6> 
             
         </div>
